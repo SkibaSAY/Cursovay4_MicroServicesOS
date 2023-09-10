daabc9231406ed952c3e420fad57d2db20061f6b
Anton Slastuhin <antonslastuhin@gmail.com>
2023-09-10 18:35:04 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Cursovay4_MicroServicesOS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
